5ec3f8ddf80491b62cf87a9897fe6bd080e63001
keshavvijay092 <159714230+keshavvijay092@users.noreply.github.com>
2025-01-09 07:38:16 +0530

    Add files via upload

---
+++ 9thJan.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int n = words.size();
+        int ans = 0;
+        int m = pref.size();
+        for(int i=0;i<n;i++)
+        {
+            if(words[i].substr(0,m) == pref) ans++;
+        }
+        return ans;
+    }
+};
